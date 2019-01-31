0b06a641d58a9caeb6f53a6d62bdff72b91de43d
Sean Jones <web@sean-jones.co.uk>
2019-01-31 17:49:21 +0000

    upload page built, added more form fields to upload component

--- app/js/file-upload.js
+++ app/js/file-upload.js
@@ -78,12 +78,12 @@ window.APP = (function (module, $) {
     }
 
     function updateUploadIndicatorText() {
-      var $indicatorText = $('.upload-indicator', $uploadComponent);
-      var filePluralized = filestoUpload.length === 1 ? ' file ' : ' files ';
+      var $text = $('.js-photo-text', $uploadComponent);
+      var $countText = $('.js-photo-count');
+      var filePluralized = filestoUpload.length === 1 ? 'photograph' : 'photographs';
       
-      var text = filestoUpload.length + filePluralized + ' to upload';
-
-      $indicatorText.text(text.trim());
+      $countText.text(filestoUpload.length);
+      $text.text(filePluralized);
     }
 
     function fileDragHover(e) {
@@ -189,7 +189,7 @@ window.APP = (function (module, $) {
           filesizes[fcnt]= file.size; //store for upload progress monitoring
 
           //ADD HTML TO THE PAGE in Specfied element
-          $('.upload-component-filelist', $uploadComponent).prepend(itemHtml);
+          $('.upload-component-filelist-inner', $uploadComponent).prepend(itemHtml);
 
           //Update the thumbnail from the local file
           displayFile(file,"img_" + idx);
@@ -198,18 +198,97 @@ window.APP = (function (module, $) {
     }
 
     function formatImgEntry(file, index) {
-      var imgThumbnail = '<img id="img_' + index +'" src="" class="upload-item-thumbnail" title="' + file.name + '">';
+      var imgThumbnail = '<div class="upload-item-thumbnail-container"><img id="img_' + index +'" src="" class="upload-item-thumbnail" title="' + file.name + '"></div>';
 
       var details= '\
-        <div class="upload-item-details"> \
-          <div class="form-field"> \
-            <label for="Img_"' + index + '"_date">Date <span class="mandatory">*</span></label> \
-            <input type="text" id="Img_"' + index + '"_date" class="datepicker"> \
-            <input type="hidden" id="Img_"' + index + '"_dateF" > \
+        <div class="form-fields upload-item-details"> \
+          <div class="form-row"> \
+            <div class="form-group col-md-6"> \
+              <label for="Img_"' + index + '"_date">Date</label> \
+              <input type="text" id="Img_"' + index + '"_date" class="form-control datepicker"> \
+              <input type="hidden" id="Img_"' + index + '"_dateF" > \
+            </div> \
+            <div class="form-group col-md-6"> \
+              <label for="Img_"' + index + '"_time">Time <span class="optional">(optional)</span></label> \
+              <input type="time" id="Img_"' + index + '"_time" class="form-control"> \
+            </div> \
           </div> \
-          <div class="form-field"> \
-            <label for="Img_"' + index + '"_time">Time <span class="optional">*</span></label> \
-            <input type="time" id="Img_"' + index + '"_time" class="datepicker"> \
+          <div class="form-row"> \
+            <div class="col-md-12"><label for="Img_"' + index + '"_area">Describe where the photo was taken</label></div> \
+            <div class="form-group col-md-6"> \
+              <select id="Img_"' + index + '"_area" class="custom-select"> \
+                <option value="none">Select General Area</option> \
+                <option value="Firth of Lorn - Insh">Firth of Lorn – Insh</option> \
+                <option value="Firth of Lorn - Kerrera">Firth of Lorn – Kerrera</option> \
+                <option value="Firth of Lorn - Mull shore">Firth of Lorn – Mull shore</option> \
+                <option value="Firth of Lorn - Morven shore">Firth of Lorn – Morven shore</option> \
+                <option value="Sound of Mull North">Sound of Mull North</option> \
+                <option value="Sound of Mull South">Sound of Mull South</option> \
+                <option value="Sound of Jura North">Sound of Jura North</option> \
+                <option value="Sound of Jura South">Sound of Jura South</option> \
+                <option value="Other">Other (Specify Below)</option> \
+              </select> \
+            </div> \
+            <div class="form-group col-md-6"> \
+              <select id="Img_"' + index + '"_location" class="custom-select"> \
+                <option value="none">Select Coordinate Entry</option> \
+                <option value="coords">Enter Coordinates or Click on Map</option> \
+                <option value="gps">Coordinates from Image EXIF</option> \
+              </select> \
+            </div> \
+          </div> \
+          <div class="form-row"> \
+            <div class="form-group col-md-6"> \
+              <label for="Img_"' + index + '"_angling">Platform <span class="optional">(optional)</span></label> \
+              <select id="Img_"' + index + '"_angling" class="custom-select"> \
+                <option value="none">Select</option> \
+                <option value="charter">Charter boat</option> \
+                <option value="private">Private boat</option> \
+                <option value="other">Other boat</option> \
+                <option value="shore">Shore</option> \
+              </select> \
+            </div> \
+            <div class="form-group col-md-6"> \
+              <label for="Img_"' + index + '"_angling">Gender <span class="optional">(optional)</span></label> \
+              <select id="Img_"' + index + '"_gender" class="custom-select"> \
+                <option value="N"></option> \
+                <option value="M">Male</option> \
+                <option value="F">Female</option> \
+                <option value="U">Don\'t know</option> \
+              </select> \
+            </div> \
+          </div> \
+          <div class="form-row"> \
+            <div class="form-group col-md-6"> \
+              <label for="Img_"' + index + '"_length">Length of fish in cm <span class="optional">(optional)</span></label> \
+              <input type="text" id="Img_"' + index + '"_length" class="form-control"> \
+            </div> \
+            <div class="form-group col-md-6"> \
+              <label for="Img_"' + index + '"_width">Width of fish in cm <span class="optional">(optional)</span></label> \
+              <input type="text" id="Img_"' + index + '"_width" class="form-control"> \
+            </div> \
+          </div> \
+          <div class="form-row"> \
+            <div class="form-group col-md-6"> \
+              <label for="Img_"' + index + '"_weight">Estimated weight in lbs <span class="optional">(optional)</span></label> \
+              <input type="text" id="Img_"' + index + '"_weight" class="form-control"> \
+            </div> \
+            <div class="form-group col-md-6" title="If the images are of diffrent skates please indicate which skate the image is of."> \
+              <label for="Img_"' + index + '"_skate">ID <span class="optional">(optional)</span></label> \
+              <input type="number" id="Img_"' + index + '"_skate" min="1" max="5" class="form-control"> \
+            </div> \
+          </div> \
+          <div class="form-row"> \
+            <div class="form-group col-md-6"> \
+              <label for="Img_"' + index + '"_tag">List any Tag numbers found <span class="optional">(optional)</span></label> \
+              <input type="text" id="Img_"' + index + '"_tag" class="form-control"> \
+            </div> \
+          </div> \
+          <div class="form-row"> \
+            <div class="form-group col-md-12"> \
+              <label for="Img_"' + index + '"_details">Any further details e.g. scars <span class="optional">(optional)</span></label> \
+              <textarea id="Img_"' + index + '"_details" class="form-control"></textarea> \
+            </div> \
           </div> \
         </div> \
       ';
